13d20f73fc1767188d71027a1875e3e8a5ad151c
root <mark.jesusmanabat@gmail.com>
2023-01-10 09:57:17 +0800

    update

---
+++ pages/api/discord-bot/commands/kanye-quote.ts
@@ -0,0 +1,28 @@
+import { APIApplicationCommandInteraction, APIInteractionResponse } from "discord-api-types/v10";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { EmbedFailedResponse } from "utils/error-handling";
+import axios, { AxiosResponse } from "axios";
+
+export const register = new SlashCommandBuilder()
+  .setName("kanye-quote")
+  .setDescription("Give's you a random kanye quotes");
+
+export const execute = async (interaction: APIApplicationCommandInteraction): Promise<APIInteractionResponse> => {
+  try {
+    const data = (await axios.get("https://api.kanye.rest/")) as AxiosResponse<{ quote: string }>;
+    return {
+      type: 4,
+      data: {
+        embeds: [
+          {
+            title: data.data.quote || "",
+            description: `- Kanye West`,
+            color: 0x1d746d,
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    return EmbedFailedResponse();
+  }
+};
